361b9cf74133064788e502304976d7f2120a4761
Choudhary Vivek Mukesh <vivek55hifi@gmail.com>
2026-08-06 22:52:49 +0530

    Time: 1 ms (100%), Space: 42.8 MB (14.66%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -1,19 +1,19 @@
 class Solution {
     public int smallestNumber(int n, int t) {
-        while (true) {
+       
+        while(true) {
             int product = 1;
             int x = n;
 
-            if (x == 0) {
+            if(x == 0) {
                 product = 0;
-            } else {
-                while (x > 0) {
+            } else{
+                while(x > 0) {
                     product *= (x % 10);
                     x /= 10;
                 }
             }
-
-            if (product % t == 0) {
+            if(product % t == 0){
                 return n;
             }
             n++;
