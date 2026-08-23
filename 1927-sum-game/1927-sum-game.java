6fd8e83508a006c319d8ec003ab287ba752aaef9
Choudhary Vivek Mukesh <vivek55hifi@gmail.com>
2026-08-23 21:11:21 +0530

    Time: 7 ms (79.56%), Space: 47.1 MB (41.61%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int half = n / 2;
+
+        int diff = 0;
+        int qDiff = 0;
+
+        for (int i = 0; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                qDiff += (i < half ? 1 : -1);
+            } else {
+                diff += (i < half ? 1 : -1) * (c - '0');
+            }
+        }
+
+        return qDiff % 2 != 0 || diff * 2 + qDiff * 9 != 0;
+    }
+}
