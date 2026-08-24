5c549fe0177c832c782306ac9618df4e39456b0c
Choudhary Vivek Mukesh <vivek55hifi@gmail.com>
2026-08-24 20:53:50 +0530

    Time: 3 ms (100%), Space: 86.5 MB (75.32%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -1,23 +1,19 @@
 class Solution {
     public int stoneGameVIII(int[] stones) {
-        int n = stones.length;
+     int n = stones.length;
 
-        // prefix sum
-        long[] prefix = new long[n];
-        prefix[0] = stones[0];
+     long[] prefix = new long[n];
+     prefix[0] = stones[0];
 
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + stones[i];
-        }
+     for(int i = 1; i<n; i++){
+        prefix[i] = prefix[i-1]+stones[i];
+     }
 
-        // dp represents the best score difference
-        // from the current state.
-        long dp = prefix[n - 1];
+     long dp = prefix[n-1];
 
-        for (int i = n - 2; i >= 1; i--) {
-            dp = Math.max(dp, prefix[i] - dp);
-        }
-
-        return (int) dp;
+     for(int i=n-2; i>=1; i--){
+        dp = Math.max(dp,prefix[i]-dp);
+     }
+     return (int) dp;
     }
 }
